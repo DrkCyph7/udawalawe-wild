2f73960bcaeb7d352fb243ee37b638666291c19d
Dinuka Lakshan <74536249+DrkCyph7@users.noreply.github.com>
2026-08-09 06:49:22 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -377,11 +377,19 @@ function RootComponent() {
           <SafariLoader visible={showReactLoader} />
 
           <div className="flex min-h-screen flex-col">
-            <SiteHeader />
-            <main className="flex-1 overflow-x-hidden relative">
-              <Outlet />
-            </main>
-            <SiteFooter />
+            {/* Main content wrapper (sits on top and scrolls normally) */}
+            <div className="relative z-10 flex-1 bg-[oklch(0.12_0.05_150)] shadow-[0_20px_40px_rgba(0,0,0,0.8)]">
+              <SiteHeader />
+              <main className="relative flex-1 overflow-x-hidden">
+                <Outlet />
+              </main>
+            </div>
+
+            {/* Sticky reveal footer (sits underneath main content) */}
+            <div className="sticky bottom-0 z-0">
+              <SiteFooter />
+            </div>
+
             <WhatsAppButton />
           </div>
         </div>
