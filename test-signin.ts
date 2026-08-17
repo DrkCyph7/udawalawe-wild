f918440803038b7f506f3c81e2859f7ce56570bb
Dinuka Lakshan <74536249+DrkCyph7@users.noreply.github.com>
2026-08-18 00:31:54 +0530

    up

--- test-signin.ts
+++
@@ -1,8 +0,0 @@
-import { signIn } from "./src/lib/auth";
-
-async function run() {
-  const result = await signIn("admin@udawalawe-wild.com", "wrongpass", null);
-  console.log(result);
-}
-
-run();
